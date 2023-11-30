2989d12ac0e28122064260c87132b5683bde0888
TheMrAleX <alejandroperezsantana55@gmail.com>
2023-11-30 09:34:24 -0500

    Version 0.1.1

--- README.md
+++ README.md
@@ -1,31 +1,15 @@
-Bienvenido a la documentación de econnect, un programa para interactuar con el Portal Cautivo de Etecsa hecho en su totalidad con Python puro, un paquete listo para instalar con `pip install` en tu ordenador, te cuento como hacerlo.
+# EConnect-module
 
-# Introducción
-El módulo econnect es una herramienta sencilla y eficaz para gestionar la conexión a la red. En esta guía rápida, te mostraremos cómo utilizarlo en tus proyectos.
-# Instalación
-Primero, instala el módulo con el siguiente comando:<br>
-`pip install econnect`<br>
-Luego, impórtalo en tu proyecto de la siguiente manera:<br>
-`from enet import econnect`<br>
-Ahora que has importado correctamente el módulo, puedes empezar a utilizarlo.
-# Uso básico
-El módulo econnect proporciona una clase llamada "nauta". Para comenzar a utilizarla, simplemente crea una instancia de la clase de la siguiente manera:<br>
-`nauta = econnect.nauta()`<br>
-Ahora estás listo para utilizar todos los métodos de la clase.
-# Flujo de trabajo
-Para un funcionamiento adecuado, sigue estos pasos:<br>
-- **Verificación de conexión:** Antes de llamar al método `login_net`, asegúrate de verificar la conexión con el método `verify_connection`. Esto se hace para validar ciertos parámetros y evitar errores. Aquí tienes un ejemplo de implementación: <br>
-```
-if nauta.verify_connection():
-    nauta.login_net('usuario@nauta.com.cu', 'password_del_usuario')
-```
-- **Obtención del tiempo disponible:** Para obtener el tiempo disponible, el usuario debe haber iniciado sesión. Puedes ejecutar el método `get_time_remaining` de la misma manera que llamamos a otros métodos. Este método devuelve el tiempo disponible y se recomienda almacenarlo en una variable:<br>
-`tiempo_disponible = nauta.get_time_remaining()`
-- **Cierre de sesión:** Para finalizar la sesión, simplemente llama al método `close_connection`:<br>
-`nauta.close_connection()`
-# Lista de métodos
-A continuación, se presenta una lista de los métodos disponibles en la clase `nauta`:
-- **verify_connection():** Este método devuelve True o False dependiendo de si la conexión con el Portal Cautivo es exitosa. Debe ejecutarse siempre en primer lugar.
-- **login_net():** Este método requiere dos parámetros: el nombre de usuario y la contraseña. Inicia sesión en la red si no hay errores.
-- **get_time_remaining():** Devuelve el tiempo disponible. Se recomienda almacenarlo en una variable para su posterior uso.
-- **close_connection():** Cierra la sesión.
+Esto es un paquete de Python que permite a los desarrolladores cubanos interactuar de manera facil y sencilla con el Portal Cautivo de Etecsa.
+
+# Caracteristicas
+- Verificación de conexión.
+- Inicio de Sesión.
+- Guardado de datos para el cierre de manera local.
+- Obtención de tiempo disponible.
+- Cierre de sesión.
+- Cierre de sesion de forma local con los datos guardados por el usuario
+
+[Repositorio del proyecto aqui](https://guides.github.com/features/mastering-markdown/)
+para los que se interesen en el código.<br>
+[Documentacion del paquete](https://github.com/TheMrAleX/econnect/blob/main/README.md) para una ayuda de como usar el paquete correctamente.
--- enet/econnect.py
+++ enet/econnect.py
@@ -1,100 +1,135 @@
-"""Esta es una libreria de Python hecha para ayudar a los desarrolladores cubanos a interactuar con la red
-Nauta de Etecsa, las utilidades son simples, es un cliente para conectarse desde consola a la red, puedes implementarlo
-en una GUI si deseas, al fin y al cabo es Open Source:
-
-#Caracteristicas:
--puedes verificar si estas conectado con la funcion 'verify_connection, devuelve True, False dependiendo la conexion'.
--puedes iniciar sesion con la funcion 'loggin_net, hay que darle el usuario y contrasena para que inicie sesion y
-retorna True, False en consecuencia si fue exitosa la operacion o no'.
--puedes obtener el tiempo disponible con la funcion 'get_time_remaining', retorna el valor del tiempo disponible y
-si hay algun error imprime en consola la excepcion.
--puedes cerrar sesion con la funcion 'close_connection', lo mismo que en get_time_remaining.
-
-#Desarrollado por TheMrAleX"""
-
-import requests
-from bs4 import BeautifulSoup as bs
+import httpx
 import re
+import json
+from bs4 import BeautifulSoup as bs
 
-
-class nauta:
+# clase principal del modulo, interactua con el portal cautivo
+class nauta():
+    # funcion de inicializacion de la clase
     def __init__(self):
-        # variables abajo
+    # variables necesarias para el funcionamiento del modulo
         self.close_data = None
         self.data_time = None
         self.attribute_uuid = None
         self.attribute_uuid_patron = None
         self.error = None
         self.errors = None
-        self.data_logged = None
+        self.data_log = None
         self.password = None
         self.username = None
-        self.data_for_login = None
+        self.login_dat = None
         self.response = None
         self.soup = None
         self.token_csrf = None
         self.wlanuserip = None
         self.logger_id = None
-        # variables arriba
-        # url que usaremos para trabajar
+        # guardamos en cada variable las respectivas urls que utilizaremos
         self.url = 'https://secure.etecsa.net:8443'
         self.url_login = 'https://secure.etecsa.net:8443/LoginServlet'
         self.url_logout = 'https://secure.etecsa.net:8443/LogoutServlet'
         self.url_query = 'https://secure.etecsa.net:8443/EtecsaQueryServlet'
-
-    def verify_connection(self):
+        # headers para que las peticiones http sean correctas
+        self.headers = {
+            "User-Agent": "Mozilla/5.0(Windows NT 10.0; Win64; x64; rv: 117.0.1)"
+        }
+        # aqui inicializamos la sesion que usaremos a lo largo del uso del modulo
+        self.cliente = httpx.Client()
+        
+    # funcion para verificar si hay conexion con la web de secure.etecsa.net:8443 (El portal cautivo)
+    def test_net(self):
         try:
-            # intentamos hacer una peticion POST a la url de Etecsa.
-            self.response = requests.get(self.url)
-            # Si la peticion va bien lo pasamos por BeautifulSoup y retornamos True.
+            # intentamos realizar una solicitud http a la url
+            self.response = self.cliente.get(self.url, timeout=5)
+            # si la solicitud va bien lo parseamos en bs4
             if self.response.status_code == 200:
                 self.soup = bs(self.response.content, 'html.parser')
                 return True
-        # Si algo va mal retornamos False.
-        except Exception as exception:
-            print(f'Error: {exception}')
+            else:
+                return False
+        except httpx.TimeoutException as e:
+            print(f'Error: {e}')
             return False
-
+        except httpx.ConnectError as e1:
+            print(f'Error: {e1}')
+            return False
+        
+    # funcion para loguearnos en la red
     def login_net(self, username, password):
         try:
-            # Intentamos buscar parametros necesarios para el inicio de sesion.
+            # buscamos parametros necesarios para el inicio de sesion
             self.token_csrf = self.soup.find('input', {'name': 'CSRFHW'}).get('value')
             self.logger_id = self.soup.find('input', {'name': 'loggerId'}).get('value')
             self.wlanuserip = self.soup.find('input', {'name': 'wlanuserip'}).get('value')
-            # Creamos un diccionario con los datos utiles para la solicitud POST que haremos posteriormente para
-            # iniciar sesion.
-            self.data_for_login = {
+            # creamos un diccionario con todos los datos para el login
+            self.login_dat = {
                 'loggerId': self.logger_id,
                 'wlanuserip': self.wlanuserip,
                 'CSRFHW': self.token_csrf,
                 'username': username,
                 'password': password
             }
+            # guardamos el usuario y el password en variables para otras peticiones
             self.username = username
             self.password = password
-
-            # Enviamos la solicitud POST con los datos del diccionario y buscamos si hay algun error con 're'
-            self.data_logged = requests.post(self.url_login, data=self.data_for_login)
-            self.errors = re.compile(r'alert\((.*?)\)')
-            self.error = re.findall(self.errors, self.data_logged.text)
-
+            # enviamos una solicitud POST para iniciar sesion con la informacion adecuada, si hay algun error lo detectamos y aislamos en una variable con expresiones regulares
+            self.data_log = self.cliente.post(self.url_login, data=self.login_dat, timeout=5, follow_redirects=True)
+            errors = re.compile(r'alert\((.*?)\)')
+            error = re.findall(errors, self.data_log.text)
             '''Si no hay errores la pagina nos devolvera una lista con todos los errores posibles
             por ende si hay 12 elementos en la lista no habra ningun error al iniciar sesion
             Aqui verificamos si la longitud de los errores es == 12 y si es True buscamos el dato 
             ATTRIBUTE_UUID necesario para obtener tiempo disponible y cerrar sesion posteriormente.'''
-            if len(self.error) == 12:
+            if len(error) == 12:
                 self.attribute_uuid_patron = r'ATTRIBUTE_UUID=([A-F0-9]+)'
-                self.attribute_uuid = re.findall(self.attribute_uuid_patron, self.data_logged.text)
+                self.attribute_uuid = re.findall(self.attribute_uuid_patron, self.data_log.text)
                 self.attribute_uuid = self.attribute_uuid[1]
-
+                 # si todo va bien retornamos True
                 return True
             else:
-                print(f'Error: {self.error}')
-
-        except Exception as e1:
-            print(f'Error: {e1}')
-
-    def get_time_remaining(self):
+                # si no va bien, retornamos el error
+                return error[2]
+        except Exception as e:
+            return e
+        
+    # funcion para guardar los datos necesarios para cerrar la sesion en caso de que se cierre el programa
+    def save_data(self, ruta):
+        try:
+            # creamos un diccionario con la estructura necesaria para el cierre de sesion
+            datos_para_cerrar = {
+                'ATTRIBUTE_UUID': self.attribute_uuid,
+                'loggerId': self.logger_id,
+                'wlanuserip': self.wlanuserip,
+                'CSRFHW': self.token_csrf,
+                'username': self.username,
+                'password': self.password,
+                'remove': 1
+            }
+            # creamos un fichero json en la ruta especificada y guardamos los datos en el
+            with open(ruta, 'w') as archivo:
+                json.dump(datos_para_cerrar, archivo)
+            # si todo va bien retornamos true
+            return True
+        except:
+            # si hay alguna excepcion retornamos False
+            print('El archivo tiene que ser json, y tiene que existir en tu disco duro dentro de el al menos deben haber {} 2 corchetes y ya')
+            return False
+        
+    # funcion para cargar los archivos guardados y los retorna a una variable
+    def load_data(self, ruta):
+        try:
+            # abrimos un archivo que haya sido guardado con la funcion save_data en una ruta especificada por el usuario
+            with open(ruta, 'r') as archivo:
+                # cargamos la informacion de ese diccionario en una variable
+                datos_cargados = json.load(archivo)
+            # si todo va bien retornamos esa variable
+            return datos_cargados
+        except:
+            # si hay alguna excepcion retornamos error_cargar_datos
+            return 'error_cargar_datos'
+        
+    # funcion para obtener el tiempo disponible despues de haber iniciado sesion
+    def get_time(self):
+        # creamos un diccionario con los datos necesarios para obtener el tiempo disponible
         self.data_time = {
             'op': 'getLeftTime',
             'ATTRIBUTE_UUID': self.attribute_uuid,
@@ -104,16 +139,18 @@ class nauta:
             'username': self.username
         }
         try:
-            '''Para obtener el tiempo disponible creamos el diccionario de arriba que contiene los datos necesarios
-            y hacemos una solicitud POST a la web de etecsa.query para consultar el tiempo disponible.'''
-            time_remaining = requests.post(self.url_query, data=self.data_time)
-            return time_remaining.text
-            # return True
-        except Exception as e2:
-            print(f'Error al obtener el tiempo de la cuenta: {e2}')
-
-    def close_connection(self):
-        self.close_data = {
+            # ahora realizamos una solicitud POST a la web de etecsa.query para que devuelva el tiempo disponible
+            time = self.cliente.post(self.url_query, data=self.data_time, timeout=5)
+            # si todo va bien retornamos el valor del tiempo
+            return time.text
+        except:
+            # si algo va mal retornamos False
+            return False
+        
+    # funcion para cerrar sesion
+    def logout(self):
+        # creamos un diccionario con los datos necesarios para cerrar la sesion
+        close_data = {
             'ATTRIBUTE_UUID': self.attribute_uuid,
             'CSRFHW': self.token_csrf,
             'wlanuserip': self.wlanuserip,
@@ -122,39 +159,54 @@ class nauta:
             'remove': 1
         }
         try:
-            # Para cerrar sesion lo mismo que para conseguir el valor de tiempo disponible :).
-            requests.post(self.url_logout, data=self.close_data)
+            # para cerrar la sesion hacemos una solicitud POST con los datos de cierre y esperar que sea OK
+            self.cliente.post(self.url_logout, data=close_data, timeout=5, follow_redirects=True)
+            # si todo va bien retornamos True
             return True
-        except Exception as e3:
-            print(f'No fue posible cerrar la sesion: {e3}')
+        except:
+            # si algo va mal retornamos False
+            return False
+    
+    # funcion para cerrar la sesion si el usuario cerro el programa pero se lograron guardar los datos
+    def logout_back(self, ruta):
+        try:
+            # cargamos los datos con la funcion load_data y hacemos la peticion POST con esos datos
+            data = self.load_data(ruta)
+            self.cliente.post(self.url_logout, data=data, timeout=5, follow_redirects=True)
+            # si todo va bien retornamos True
+            return True
+        except:
+            # si algo va mal retornamos False
+            return False
+        
+    '''cuando el usuario cierra el programa sin cerrar sesion y quiere recuperar la sesion,
+    podemos hacerlo pero hay que volver a obtener el tiempo restante, esta funcion hace ese
+    trabajo'''
+    def reanude_login(self, ruta):
+        try:
+            # cargamos los datos con la funcion load_data
+            datos = self.load_data(ruta)
+            # borramos estos datos del diccionario ya que no lo necesitamos
+            del datos['password']
+            del datos['remove']
+            # agregamos el dato op que si es necesario
+            datos['op'] = 'getLeftTime'
+            time = self.cliente.post(self.url_query, data=datos, timeout=5)
+            # si todo va bien retornamos el tiempo disponible
+            return time.text
+        except:
+            # si algo va mal retornamos False
             return False
 
+'''# este es un bloque de codigo para ejecutar las funciones del modulo
+nauta = nauta()
+# nauta.logout_back('d.json')
+nauta.test_net()
+nauta.test_net()
+nauta.login_net('leonel.perez77@nauta.com.cu', 'UpaNvbr8.1212')
+import time
+nauta.save_data('d.json')
+'''
 
-if __name__ == '__main__':
-    print('Bienvenido, inicia sesion aqui\n')
-    econnect = nauta()
 
-    if econnect.verify_connection():
-
-        while True:
-            usernames = input('Usuario: ')
-            passwords = input('Password: ')
-
-            if econnect.login_net(usernames, passwords):
-                break
-            else:
-                pass
-
-        print(f'\nTiempo disponible: {econnect.get_time_remaining()}')
-
-        while True:
-            try:
-                a = int(input('Ingrese #1 para cerrar la sesion: '))
-                if a == 1:
-                    if econnect.close_connection():
-                        print('\nSesion cerrada con exito')
-                        break
-                    else:
-                        print('Ingresa el #1 por favor')
-            except ValueError as e:
-                print('Ingresa el #1 por favor')
+# ya funciona wiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
